4a5d252b92ec580034e22aedebddc43f28faa0b3
Wasuwat Limsuparhat <wasuwat.se@gmail.com>
2019-02-26 16:28:37 +0700

    import header component

--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { WelcomePage } from './welcome.page';
+import { HeaderComponent } from 'src/app/components/header/header.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [WelcomePage]
+  declarations: [WelcomePage, HeaderComponent]
 })
-export class WelcomePageModule {}
+export class WelcomePageModule { }
